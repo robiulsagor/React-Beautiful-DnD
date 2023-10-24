5890b4a841c39bb0ebf6957ab8fd7ffd472f2cc7
robiulsagor <robiul100.me@gmail.com>
2023-10-24 11:59:41 +0600

    first commit

---
+++ utils/initial-data.ts
@@ -0,0 +1,8 @@
+const initialData = {
+    tasks: {
+        'task-1': { id: "task-1", content: "Take out the garbase" },
+        'task-2': { id: "task-2", content: "Watch my favourite show" },
+        'task-3': { id: "task-3", content: "Take out the garbase" },
+        'task-4': { id: "task-4", content: "Take out the garbase" },
+    }
+}
